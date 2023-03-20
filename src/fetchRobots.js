870ed977e272c628ff5c436a2a4297b4c1df48c7
João Feitoza <joaofeitoza.13@gmail.com>
2023-03-19 22:51:09 -0300

    feat: add fetch to roboHash api to add nice picture to randomUser api data

--- src/fetchRobots.js
+++ src/fetchRobots.js
@@ -1,23 +1,37 @@
-const fetchRobots = async (number) => {
-  const randomUserData = await fetch(`https://randomuser.me/api/?results=${number}`)
+const fetchRobots = async (num) => {
+  const randomUserData = await fetch(
+    `https://randomuser.me/api/?results=${num}`
+  )
   const randomUserJSONData = await randomUserData.json()
   const randomUsers = randomUserJSONData.results.map(
     ({
-      name,
+      name: { first, last },
       gender,
       email,
       login: { uuid, username },
       location: { city, country },
     }) => ({
+      photo: '',
       uuid,
       username,
-      name,
+      name: `${first} ${last}`,
       gender,
       email,
       city,
       country,
     })
   )
+
+  const getRoboHashUrl = (id) => `https://robohash.org/${id}.png`
+
+  const generateRoboHashPromises = (num, users) =>
+    Array(num).map((_, index) =>
+      fetch(getRoboHashUrl(users[index].uuid)).then(
+        (response) => (users[index].photo = response.url)
+      )
+    )
+
+  await Promise.all(generateRoboHashPromises(num, randomUsers))
 
   return randomUsers
 }
