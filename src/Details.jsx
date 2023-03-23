8d309602d524e17a47b877034ecfa0bfc52d967f
João Feitoza <joaofeitoza.13@gmail.com>
2023-03-23 00:44:31 -0300

    feat: add robot data to the component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,16 +1,24 @@
 import { useContext } from 'react'
-import { useParams } from 'react-router'
+// import { useParams } from 'react-router'
 import { RobotsContext } from './RobotContext'
 
 function Details() {
-  const { id } = useParams()
+  // const { id } = useParams()
   const [robot] = useContext(RobotsContext)
-
-  console.log(robot)
-
   return (
-    <div>
-      <h1>{id}</h1>
+    <div className="tc bg-light-green dib br3 pa4 ma4 shadow-5">
+      <div className="user-image">
+        <img src={`${robot.photo}`} alt="" />
+      </div>
+      <h2 className="user-name">{robot.username}</h2>
+      <p className="about-user">{`My name is ${robot.name} and I'm a robot frontend web developer`}</p>
+      <p>{`I'm from ${robot.city}, ${robot.country}`}</p>
+      <div className="social-container">
+        <span className="">{`I have ${Math.abs(
+          Math.random() * 1000
+        ).toFixed()} projets`}</span>
+      </div>
+      <button className="hire-me">Hire Me</button>
     </div>
   )
 }
