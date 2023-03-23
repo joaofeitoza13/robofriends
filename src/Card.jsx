6ca9ebafdf8d0ed8e4a9dd7d076835393df5fc7b
João Feitoza <joaofeitoza.13@gmail.com>
2023-03-23 00:43:20 -0300

    feat: add useContext to modify context state and refactor prop usage

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { RobotsContext } from './RobotContext'
 
-const Card = (props) => {
+const Card = ({ robot }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [_, setRobot] = useContext(RobotsContext)
   return (
-    <Link to={`/details/${props.id}`}>
-      <div
-        id="card"
-        className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5"
-      >
-        <img alt="robots" src={`https://robohash.org/${props.id}?200x200`} />
+    <Link
+      to={`/details/${robot.uuid}`}
+      onClick={() => {
+        setRobot(robot)
+      }}
+    >
+      <div id="card" className="tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5">
+        <img alt="robots" src={`${robot.photo}`} />
         <div>
-          <h2>{props.username}</h2>
-          <p>{props.name}</p>
+          <h2>{robot.username}</h2>
+          <p>{robot.name}</p>
         </div>
       </div>
     </Link>
