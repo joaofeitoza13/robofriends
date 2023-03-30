d044416cf7a05bf652f95e5164978dded8f973e2
João Feitoza <joaofeitoza.13@gmail.com>
2023-03-30 00:43:35 -0300

    feat: add StrictMode and some repositioning of imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
+import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Robots from './Robots'
+import { RobotsContext } from './RobotsContext'
 import Details from './Details'
+import Robots from './Robots'
 import 'tachyons'
 import './index.css'
-import { useState } from 'react'
-import { RobotsContext } from './RobotsContext'
 
 const App = () => {
   const robot = useState(null)
 
   return (
-    <div className="tc">
-      <BrowserRouter>
-        <Link to="/">
-          <h1 id="robot-title" className="font-face">
-            Hire Dev Robots
-          </h1>
-        </Link>
-        <RobotsContext.Provider value={robot}>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<Robots />} />
-          </Routes>
-        </RobotsContext.Provider>
-      </BrowserRouter>
-    </div>
+    <React.StrictMode>
+      <div className="tc">
+        <BrowserRouter>
+          <Link to="/">
+            <h1 id="robot-title" className="font-face">
+              Hire Dev Robots
+            </h1>
+          </Link>
+          <RobotsContext.Provider value={robot}>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<Robots />} />
+            </Routes>
+          </RobotsContext.Provider>
+        </BrowserRouter>
+      </div>
+    </React.StrictMode>
   )
 }
 
